02da74224d090cabac1fc4312f25b9e90c8614d5
Thomas J. Creedy <thomas@tjcreedy.co.uk>
2021-01-05 17:35:09 +0000

    Uncommented line

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-#Rscript -e "install.packages(c('ape', 'getopt', 'geiger', 'taxize', 'plyr'), repos = 'https://cloud.r-project.org')"
+Rscript -e "install.packages(c('ape', 'getopt', 'geiger', 'taxize', 'plyr'), repos = 'https://cloud.r-project.org')"
 if (( $EUID == 0 )); then
     find *.R -maxdepth 1 -executable -type f | while read l; do ln -s $PWD/$l /usr/local/bin/$l; echo "Created symlink for $l"; done
 else
