4e65e135fb3ad6786a38583c09484e5341e4b890
Thomas J. Creedy <thomas@tjcreedy.co.uk>
2021-01-05 11:27:01 +0000

    Initial commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+#Rscript -e "install.packages(c('ape', 'getopt', 'geiger', 'taxize', 'plyr'), repos = 'https://cloud.r-project.org')"
+if (( $EUID == 0 )); then
+    find *.R -maxdepth 1 -executable -type f | while read l; do ln -s ./$l /usr/local/bin/$l; echo "Created symlink for $l" done
+else
+    echo "Run as root to create symlinks"
+fi
+
