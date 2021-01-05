17c09d9a2c77e73c8a5c40b1e8fdd5df4642e845
Thomas J. Creedy <thomas@tjcreedy.co.uk>
2021-01-05 11:36:21 +0000

    Initial commit

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 if (( $EUID == 0 )); then
-        find *.R -maxdepth 1 -executable -type f | while read l; do rm /usr/local/bin/$l; echo "Removed symlink for $l"; done
+        find *.R -maxdepth 1 -executable -type f | while read l; do rm /usr/local/bin/$l; echo "Removed symlink for $l" done
 else
     echo "Run as root to remove symlinks"
 fi
