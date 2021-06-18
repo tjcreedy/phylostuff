1b10c1861066a6a4892a4f61ea9d08fdf4c29914
tjcreedy <tjcreedy@users.noreply.github.com>
2021-06-18 14:31:48 +0100

    Possible fix for unneeded wildcard replacement in Rscript command

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-Rscript -e "x<-c('ape', 'getopt', 'geiger', 'taxize', 'plyr');install.packages(x[!x %in% installed.packages()[,'Package']], repos = 'https://cloud.r-project.org')"
+Rscript -e 'x<-c("ape", "getopt", "geiger", "taxize", "plyr");install.packages(x[!x %in% installed.packages()[,"Package"]], repos = "https://cloud.r-project.org")'
 if (( $EUID == 0 )); then
     find *.R -maxdepth 1 -executable -type f | while read l; do ln -s $PWD/$l /usr/local/bin/$l; echo "Created symlink for $l"; done
 else
