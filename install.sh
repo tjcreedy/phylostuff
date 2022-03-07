a586f7abaa0f93a0a1bb89cd71c421f3fe318e03
tjcreedy <thomas.creedy@gmail.com>
2022-03-07 14:15:24 +0000

    Added non-sudo install route

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ Rscript -e 'x<-c("ape", "getopt", "geiger", "taxize", "plyr");install.packages(x
 if (( $EUID == 0 )); then
     find *.R -maxdepth 1 -executable -type f | while read l; do ln -s $PWD/$l /usr/local/bin/$l; echo "Created symlink for $l"; done
 else
-    echo "Run as root to create symlinks"
+    echo "export PATH=$PATH:$PWD" >> ~/.bashrc
+    source ~/.bashrc
+    echo "PATHs installed, you may need to log out and in again for this to take effect"
 fi
 
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
 if (( $EUID == 0 )); then
-        find *.R -maxdepth 1 -executable -type f | while read l; do rm /usr/local/bin/$l; echo "Removed symlink for $l" done
+   find *.R -maxdepth 1 -executable -type f | while read l; do rm /usr/local/bin/$l; echo "Removed symlink for $l" done
 else
-    echo "Run as root to remove symlinks"
+   sed -i "/^export .*${PWD}$/d" ~/.bashrc
+   echo "PATHs removed, you may need to log out and in again for this to take effect"
 fi
 
